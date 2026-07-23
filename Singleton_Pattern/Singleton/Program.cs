1245e70d3d7465be2137d33f1b41b6e9de110248
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2026-07-23 12:19:48 -0300

    Singleton pattern in C#, thread safe

---
+++ Singleton_Pattern/Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using Singleton;
+
+Thread t1 =new Thread(() =>
+{
+    var instance = UploadService.Instance(1);
+});
+
+Thread t2 =new Thread(() =>
+{
+    var instance = UploadService.Instance(2);
+});
---
+++ Singleton_Pattern/Singleton/UploadService.cs
@@ -0,0 +1,26 @@
+namespace Singleton;
+
+sealed class UploadService
+{
+    public int Id { get; set; }
+    private UploadService() { }
+
+    private static UploadService _instance;
+private static readonly object _lock = new object();
+
+    public static UploadService Instance (int id)
+    {
+        if (_instance == null)
+        {
+            lock(_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new UploadService();
+                    _instance.Id = id;
+                }
+            }
+        }
+        return _instance;
+    }
+}
