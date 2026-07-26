565d567274e7c01b6e0e5132e7586ffd3249c177
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2026-07-26 15:54:38 -0300

    folder and project waiting for the builder pattern

--- Singleton_Pattern/Singleton/obj/Debug/net10.0/Singleton.AssemblyInfo.cs
+++ Singleton_Pattern/Singleton/obj/Debug/net10.0/Singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1245e70d3d7465be2137d33f1b41b6e9de110248")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
