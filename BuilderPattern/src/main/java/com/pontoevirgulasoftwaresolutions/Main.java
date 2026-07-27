af1f54809e289b8aa6f12db369a8424178e5975e
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2026-07-27 15:37:04 -0300

    testing the PdfReportBuilder

--- BuilderPattern/src/main/java/com/pontoevirgulasoftwaresolutions/Main.java
+++ BuilderPattern/src/main/java/com/pontoevirgulasoftwaresolutions/Main.java
@@ -1,17 +1,19 @@
 package com.pontoevirgulasoftwaresolutions;
 
+import com.pontoevirgulasoftwaresolutions.builders.PdfReportBuilder;
+import com.pontoevirgulasoftwaresolutions.director.ReportDirector;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        ReportDirector director = new ReportDirector();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        PdfReportBuilder pdfBuilder = new PdfReportBuilder("teste.pdf");
+
+        director.construct(pdfBuilder);
+
+        pdfBuilder.getResult();
+
     }
 }
